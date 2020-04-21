a097e95cc5c67e537e7dc05deb4d3553d63db4f3
auliakbarh <auliaakbarharahaap@gmail.com>
2020-04-21 14:15:30 +0700

    reroute app

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import RouterApp from './src/router';
 import {name as appName} from './app.json';
+
+const App = RouterApp({database: null});
 
 AppRegistry.registerComponent(appName, () => App);
